9d6b03cea4889395399b06fb6464f104d313adb8
MMBICS <phoeyarzar.ygn@gmail.com>
2023-10-22 21:24:26 +0630

    Initial Commit

---
+++ android/app/src/main/java/com/duke/bravo_fcm/MainActivity.java
@@ -0,0 +1,6 @@
+package com.duke.bravo_fcm;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/constants.dart
@@ -0,0 +1,4 @@
+class Constants {
+  static const String baseURL =
+      "https://fcm-worker.du-math-ak190.workers.dev/fcm/send";
+}
